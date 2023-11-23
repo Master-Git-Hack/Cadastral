7c2b0cc5315eff35b18960389dca9b25980f3e2b
Einar Jhordany Serna Valdivia <e.serna.1a.43@gmail.com>
2023-11-23 15:27:46 -0600

    fix axios post data

--- Backend/src/api/parse.py
+++ Backend/src/api/parse.py
@@ -4,6 +4,7 @@ from xml.etree.ElementTree import fromstring, parse
 
 from flasgger import swag_from
 from flask import Blueprint, request
+from flask_jwt_extended import jwt_required
 from xmltodict import unparse
 
 from .. import config, logger
@@ -18,9 +19,15 @@ __swagger: dict = config.API_MODELS.get("parse_files", {})
 
 @parse_files.post("/xml-to-json")
 @swag_from(__swagger.get("xml_to_json", {}))
+@jwt_required()
 def xml_to_json(response=Responses()):
-    file = request.files.get("file")
-    if file is None or file.filename == "":
+    if "file" not in (file := request.files):
+        return response.error(
+            message="No se ha enviado ningún archivo", status_code=400
+        )
+
+    file = file["file"]
+    if file.filename == "":
         return response.error(
             message="No se ha enviado ningún archivo", status_code=400
         )
@@ -32,13 +39,15 @@ def xml_to_json(response=Responses()):
         xml = file.read()
 
         root_xml = fromstring(xml)
-        data = {
-            element.tag.lower(): float(element.text)
-            if element.text.isdigit()
-            else element.text.strip().replace("\n", "")
-            for element in root_xml.iter()
-        }
-        return response.success(data=data)
+
+        return response.success(
+            data={
+                element.tag.lower(): float(element.text)
+                if element.text.isdigit()
+                else element.text.strip().replace("\n", "")
+                for element in root_xml.iter()
+            }
+        )
     except Exception as e:
         logger.error("Error al convertir el archivo XML a JSON: %s", e)
         return response.error(message=str(e), status_code=400)
@@ -46,6 +55,7 @@ def xml_to_json(response=Responses()):
 
 @parse_files.post("/json-to-xml/<string:filename>")
 @swag_from(__swagger.get("json_to_xml", {}))
+@jwt_required()
 def json_to_xml(filename: str, response=Responses()):
     data = request.get_json()
     if data is None:
