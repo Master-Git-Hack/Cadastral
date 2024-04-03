63ddcc34595827182a4bd3b4111d82c28245ab1a
Einar Jhordany Serna Valdivia <e.serna.1a.43@gmail.com>
2024-04-03 14:23:58 -0600

    updated

--- Frontend/src/store/api/baseUrl.ts
+++ Frontend/src/store/api/baseUrl.ts
@@ -38,7 +38,7 @@ const axiosBaseQuery =
 
 				if (token) {
 					headers["Authorization"] = `Bearer ${token}`;
-					headers["Protected"] = true;
+					//headers["Protected"] = true;
 					//headers["withCredentials"] = true;
 				}
 			}
